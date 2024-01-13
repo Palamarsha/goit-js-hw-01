75e4f120e98e90b864d752291cc6f70e031ec9af
Palamarsha <149620290+Palamarsha@users.noreply.github.com>
2024-01-13 17:44:03 +0200

    add in index.html right way to js.file

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,8 @@
-function getShippingMessage ()
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    console.log(`Shipping to ${country} will cost ${totalPrice} credits`)
+    
+}
+getShippingMessage("Australia", 120, 50);
+getShippingMessage("Germany", 80, 20);
+getShippingMessage("Sweden", 100, 20);
